5cdca141f79f328f75ff06c7b05c7434c2cc8b34
Mateus Valgueiro <mvt@cesar.org.br>
2021-09-03 12:03:39 -0300

    add freeport command

--- valg/main.py
+++ valg/main.py
@@ -1,5 +1,9 @@
 import typer
 
+from psutil import process_iter, AccessDenied
+from signal import SIGTERM # or SIGKILL
+
+
 app = typer.Typer()
 
 @app.command()
@@ -11,4 +15,20 @@ def callback():
     """
     Valg utils
     """
-
+    
+@app.command()
+def freeport(port: int):
+    """
+    Free port from running processes
+    """
+    for proc in process_iter():
+        try:  
+            for conns in proc.connections(kind='inet'):
+                if conns.laddr.port == port:
+                    proc.send_signal(SIGTERM)
+                    typer.echo('Process {}: {} - stopped'.format(proc.name(), proc.pid))
+        except (PermissionError, AccessDenied): 
+            pass
+        
+    typer.echo('✅ Port {} is free now'.format(port))
+    
